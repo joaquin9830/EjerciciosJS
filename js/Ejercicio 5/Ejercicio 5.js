a616590913e6d99be16fbcdcaa655aef88571087
joaquin9830 <joaquin9811@gmail.com>
2022-10-20 18:58:12 -0300

    Ejercicio 5

---
+++ js/Ejercicio 5/Ejercicio 5.js
@@ -0,0 +1,18 @@
+//Altura en centímetros
+let altura = 168;
+console.log(altura)
+//Altura en metros
+let alturaM = 1.68;
+
+//Peso en Kilogramos;
+let pesoK = 65;
+
+//Altura en metros redondeada hacia arriba
+let alturaArriba = Math.ceil(alturaM)
+
+//Peso en kilogramos redondeado hacia abajo
+let pesoAbajo = Math.floor(pesoK)
+
+//Máximo de javascript
+
+let MaximoJ = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
